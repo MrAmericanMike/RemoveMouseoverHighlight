0fa1c523914e94ff5e1bab630744aed72fe7c514
MrAmericanMike <mramericanmike@gmail.com>
2021-12-15 15:24:38 -0300

    Code cleanup and logo added

--- src/main/java/mramericanmike/rmh/RMH.java
+++ src/main/java/mramericanmike/rmh/RMH.java
@@ -6,18 +6,22 @@ import mramericanmike.rmh.keybinds.MyKeyMappings;
 import mramericanmike.rmh.setup.ModConfigs;
 import mramericanmike.rmh.setup.ModInfo;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(ModInfo.MOD_ID)
 public class RMH
 {
     public RMH() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-        MinecraftForge.EVENT_BUS.register(this);
+        //MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void clientSetup(final FMLClientSetupEvent event)
