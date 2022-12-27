ea152e319c5d44cdeb6318b436e780e3d7b0b0a1
MrAmericanMike <mramericanmike@gmail.com>
2022-12-26 21:30:36 -0300

    1.19.2

--- src/main/java/mramericanmike/rmh/RMH.java
+++ src/main/java/mramericanmike/rmh/RMH.java
@@ -2,13 +2,16 @@ package mramericanmike.rmh;
 
 import mramericanmike.rmh.setup.ModConfigs;
 import mramericanmike.rmh.setup.ModInfo;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(ModInfo.MOD_ID)
 public class RMH {
 	public RMH(){
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModConfigs.SPEC, "RemoveMouseoverHighlight-client.toml");
 	}
 }
